8c7ebed2018860481f5252a632f5083b7a8b8d36
Hzainn17 <hilmazain17@gmail.com>
2024-05-14 21:58:42 +0700

    test

--- app/src/main/java/com/example/zerentapp/MainActivity.kt
+++ app/src/main/java/com/example/zerentapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Ridea Android")
+                    Greeting("Hilma Android")
                 }
             }
         }
