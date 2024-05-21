4e7b250c4cccb6505d205370911db26a6f9c4cd7
Hendra Dwi Prastia <hendrapang888@gmail.com>
2024-05-21 18:55:45 +0700

    chat first test

--- app/src/main/java/com/example/zerentapp/chatDetail.kt
+++ app/src/main/java/com/example/zerentapp/chatDetail.kt
@@ -5,12 +5,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,7 +54,7 @@ fun ChatDetail(
                 color = Color.White,
                 modifier = Modifier
 
-                    .padding(top = 8.dp, bottom = 8.dp,start = 16.dp, end = 16.dp)
+                    .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp)
             )
         }
         Box(modifier = modifier) {
@@ -60,14 +69,15 @@ fun ChatDetail(
                     .height(60.dp)
 
 
-
             ) {
-                Text(text = "This is a pointy chat box.",
-                        color = Color.White,
-                        fontWeight = FontWeight.Medium,
-                        modifier = Modifier
+                Text(
+                    text = "This is a pointy chat box.",
+                    color = Color.White,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier
 
-                        .padding(top = 8.dp, bottom = 8.dp,start = 16.dp, end = 16.dp))
+                        .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp)
+                )
             }
 
             Text(
@@ -90,14 +100,15 @@ fun ChatDetail(
                     .height(60.dp)
 
 
-
             ) {
-                Text(text = "p.",
+                Text(
+                    text = "p.",
                     color = Color.White,
                     fontWeight = FontWeight.Medium,
                     modifier = Modifier
 
-                        .padding(top = 8.dp, bottom = 8.dp,start = 16.dp, end = 16.dp))
+                        .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp)
+                )
             }
 
             Text(
@@ -107,6 +118,44 @@ fun ChatDetail(
                     .align(Alignment.BottomEnd)
                     .padding(8.dp)
             )
+        }
+        Surface(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+        ) {
+            ChatBottomBar(onSendClick = { message -> /* Send message */ })
+        }
+    }
+    }
+
+
+@Composable
+fun ChatBottomBar(
+    modifier: Modifier = Modifier,
+    onSendClick: (String) -> Unit
+) {
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(height = 80.dp, width = 200.dp),
+    ) {
+        BottomAppBar(
+            containerColor = Color(0xFF043C5B),
+            contentColor = Color.White,
+        ) {
+            TextField(
+                value = "",
+                shape = RoundedCornerShape(20.dp),
+                onValueChange = {},
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(top=3.dp, bottom=2.dp, start=10.dp, end=10.dp),
+                placeholder = { Text("Type a message") }
+            )
+            IconButton(onClick = { onSendClick("Your message") }) {
+                Icon(imageVector = Icons.Filled.Send, contentDescription = "Send")
+            }
         }
     }
 }
